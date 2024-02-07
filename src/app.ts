ebfb9e05e6037c11dc15ae5e7a09a15e323d7e64
Pierre Doyeux <pierre.doyeux@skopai.com>
2024-02-07 10:53:45 +0100

    express: add dumb router+controller

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,11 @@
 import "dotenv/config";
-import { singletons } from "./inversify.config.js";
-import { TYPES } from "./types.js";
 import express from "express";
-import { DumbAService, DumbBService } from "./services/index.js";
 import { HTTP_SERVER_PORT } from "./models/env.model.js";
+import { appRouter } from "./routers/appRouter.js";
 
 const app = express();
-const dumbAService = singletons.get<DumbAService>(TYPES.DumbAService);
-dumbAService.log();
-const dumbBService = singletons.get<DumbBService>(TYPES.DumbBService);
-dumbBService.log();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to your app!");
-});
+app.use("/", appRouter);
 
 app.listen(HTTP_SERVER_PORT, () => {
   console.log(`server listening on port ${HTTP_SERVER_PORT}`);
